297cc819bcaf62de9e9bd282401173f30a36a294
Evelina Alimaite <alimaitevelina@gmail.com>
2013-04-11 13:09:28 +0300

    Realizuota pirmine versija be patikrinimo

--- AdaptyvusMonteCarlo2.m
+++ AdaptyvusMonteCarlo2.m
@@ -1,18 +1,43 @@
 % Adaptyvi Monte Carlo (Random Search) realizacija
 %1. Sugeneruojame 100 atsitiktiniu tasku int [-10.10]^2
 %2. Surandame, kuriame funkcija igyja didziausia (maziausia) reiksme
-a=-10;
-b=10;
+a=-10; %pradine sritis
+b=10; %pradine sritis
 n=2;% Dimensija (matavimas)
-k=1000;% tasku (vektoriu) skaicius
-x=a + (b-a).*rand(k,n);% Perdaryti kad generuotu dvimacius
-for i=1:k
-  f(i)=sincos2(x(i,:));
+k1=50;% tasku (vektoriu) skaicius
+x1=a + (b-a).*rand(k1,n);% Perdaryti kad generuotu dvimacius
+f=[];
+for i=1:k1
+  f(i)=sincos2(x1(i,:));
 end
-[fMin,indMin]=min(f);
-[fMax,indMax]=max(f);
-xMin=x(indMin,:);
-xMax=x(indMax,:);
-fprintf('Surastas min=%6.4f taske x=(%6.4f,%6.4f)\n',fMin,xMin(1),xMin(2));
-fprintf('Surastas max=%6.4f taske x=(%6.4f,%6.4f)\n',fMax,xMax(1),xMax(2));
-%grafikas2
+[fMin1,indMin1]=min(f);
+%[fMax1,indMax1]=max(f);
+xMin1=x1(indMin1,:);
+%xMax1=x(indMax1,:);
+fprintf('Surastas min=%6.4f taske x=(%6.4f,%6.4f)\n',fMin1,xMin1(1),xMin1(2));
+%fprintf('Surastas max=%6.4f taske x=(%6.4f,%6.4f)\n',fMax1,xMax1(1),xMax1(2));
+hold on;
+scatter(x1(:,1),x1(:,2),'b.');
+scatter(xMin1(1),xMin1(2),'r*');
+text(xMin1(1)+0.3,xMin1(2),num2str(fMin1));
+rectangle('Position',[-10.0,-10.0,20.0,20.0],...
+    'LineWidth',5,'LineStyle','--')
+
+%sumazinta sritis
+%{
+c=xMin1(1)-2; %sumazinta sritis
+d=xMin1(2)-2; %sumazinta sritis
+k2=50;% tasku (vektoriu) skaicius
+x2=c + (d-c).*rand(k2,n);% Perdaryti kad generuotu dvimacius
+f2=[];
+for i=0:k2
+  f2(i)=sincos2(x2(i,:));
+end
+[fMin2,indMin2]=min(f2);
+%[fMax2,indMax2]=max(f2);
+xMin2=x(indMin,:);
+%xMax2=x2(indMax,:);
+fprintf('Surastas min=%6.4f taske x2=(%6.4f,%6.4f)\n',fMin2,xMin2(1),xMin2(2));
+%fprintf('Surastas max=%6.4f taske x=(%6.4f,%6.4f)\n',fMax2,xMax2(1),xMax2(2));
+%grafikas2 
+%}
